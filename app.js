273dbf6427ef0a5ff384a662f80123cad5025036
Babar Hans <babarhans34@gmail.com>
2023-11-22 14:59:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// function myfunction(p1,p2){
+//    return p1*p2;
+// }
+
+
+// let result = myfunction(2,2)
+// document.getElementById('demo').innerHTML = `The Two Number Product are = ${result}`;
+
+
+// const person = {
+//     firstName: "Babar",
+//     lastName: "Hans",
+//     iD: 1313,
+    
+//     // that is called lambda function
+//     fullName: function(){
+//         return this.firstName + " " + this.lastName;
+//     }                              
+// };                 
+// //when function calls with object that function is coverted into method
+
+// let x = person.fullName()
+// document.getElementById('demo').innerHTML=x;
+
+
+const pi = 3.141592653589793238462643383279502884197;
+let r = 4;
+let pi = 4;
+console.log(pi*r*r);
